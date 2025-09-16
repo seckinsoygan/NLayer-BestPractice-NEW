935a1bb23a6b0c77a363d682a896bdba7d905888
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2025-09-16 14:40:25 +0300

    IGenericRepo and GenericRepo added

---
+++ Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Repositories.Interfaces;
+public interface IGenericRepository<T> where T : class
+{
+    IQueryable<T> GetAll();
+    IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+    ValueTask<T?> GetByIdAsync(int id);
+    ValueTask AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
--- Repositories/Products/Product.cs
+++ Repositories/Products/Product.cs
@@ -1,4 +1,4 @@
-﻿namespace Repositories;
+﻿namespace Repositories.Products;
 public class Product
 {
     public int Id { get; set; }
